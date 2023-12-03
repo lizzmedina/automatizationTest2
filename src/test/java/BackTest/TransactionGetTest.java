f878d84d1bcfaf259902c063d16553e750dc532c
lizz medina <lizzmedina15@gmail.com>
2023-12-03 14:43:03 -0500

    try options

--- src/test/java/BackTest/TransactionGetTest.java
+++ src/test/java/BackTest/TransactionGetTest.java
@@ -9,7 +9,7 @@ import org.testng.Assert;
 public class TransactionGetTest {
     @Test
     public void GetTransactions_test_1() {
-        Response resGet = RestAssured.get("https://parabank.parasoft.com/parabank/services/bank/accounts/16452/transactions/month/All/type/All");
+        Response resGet = RestAssured.get("https://parabank.parasoft.com/parabank/services/bank/accounts/13788/transactions/month/All/type/All");
         System.out.println("El código obtenido es: " + resGet.statusCode());
         System.out.println("Se demoró: " + resGet.getTime() + " milisegundos");
         System.out.println(resGet.getBody().asString());
--- src/test/java/BackTest/TransferPostTest.java
+++ src/test/java/BackTest/TransferPostTest.java
@@ -1,6 +1,7 @@
 package BackTest;
 
 import com.google.gson.JsonObject;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,9 +10,10 @@ import static org.hamcrest.Matchers.equalTo;
 public class TransferPostTest {
     @Test
     public void PostTransfer_test_1() {
+
         given()
                 .contentType("application/json")
-                .when().post("https://parabank.parasoft.com/parabank/services/bank/transfer?fromAccountId=13788&toAccountId=17784&amount=100")
+                .when().post("https://parabank.parasoft.com/parabank/services/bank/transfer?fromAccountId=15120&toAccountId=13788&amount=5")
                 .then().statusCode(200)
                 .log().status()
                 .log().body();
